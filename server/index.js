c0c5464a83707fbc53ccfae0f789468682a7ce40
Nandhu <148442024+nandhuknair@users.noreply.github.com>
2024-10-03 20:40:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,10 +8,11 @@ const {app, server}  = require('./socket/index')
 // const app = express()
 
 app.use(cors({
-    origin:process.env.FRONTEND_URL,
-    methods: ["POST","GET"],
-    credentials:true
-}))
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
 
 app.use(express.json())
 app.use(cookiesParser())
