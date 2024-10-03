1355fc3c51e1d328a5d51cc651a8938a8b41aba1
Nandhu <148442024+nandhuknair@users.noreply.github.com>
2024-10-03 20:42:08 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,6 +14,16 @@ app.use(cors({
   credentials: true,
 }));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
 app.use(express.json())
 app.use(cookiesParser())
 
